41f1515e3883b807c782b253fb2640e10343d068
Christopher Giroir <kelsin@valefor.com>
2011-08-02 23:38:44 -0400

    Simple logging in stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate!
+
+  helper_method :current_user
   protect_from_forgery
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate!
+    redirect_to new_session_path unless session[:user_id]
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authenticate!
+
   def create
+    account = SmfAccount.authenticate(params[:username], params[:password])
+
+    if account
+      user = account.user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      session[:user_id] = nil
+      render :action => :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
   end
 end
